9e3f97aa3b9e43c5b3d355cf531f9d2f57fd79a3
Daniel Tkocz <dtk@dtk-G31M-ES2L.(none)>
2013-12-16 10:15:21 +0100

    deleted meta files; added Factories.cs

---
+++ Assets/Factories.cs
@@ -0,0 +1,16 @@
+enum Factories
+{
+	CoalMine,
+	IronMine,
+	CopperMine,
+	LeadMine,
+	MagnesiumMine,
+	SulfurMine,
+	SalpeterMine,
+	Woodcutter,
+	MetalFactory,
+	CelluloseFactory,
+	Smith,
+	Chemist,
+	College
+}
